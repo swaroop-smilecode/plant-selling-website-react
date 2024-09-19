0c0a6506e5a672b10ddf6f62cde53c2800ca15b5
swaroop <swaroopiit@gmail.com>
2024-09-19 09:04:50 +0530

    Project setup

---
+++ src/pages/auth/SignInPage.jsx
@@ -0,0 +1,10 @@
+function SignInPage() {
+    return (
+        <div className="font-playfair text-green-500">
+            Sign In!
+            <i class="fa-regular fa-face-smile"></i>
+        </div>
+    )
+}
+
+export default SignInPage
---
+++ src/pages/auth/SignUpPage.jsx
@@ -0,0 +1,9 @@
+function SignUpPage() {
+    return (
+        <div className="font-lato text-blue-500">
+            Sign Up! 
+        </div>
+    )
+}
+
+export default SignUpPage
