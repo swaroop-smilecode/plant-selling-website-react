17d9f19baf1fd876d204d6bbf50345186a4662ff
swaroop <swaroopiit@gmail.com>
2024-09-20 08:47:27 +0530

    Sign in & Sign up pages are designed & they are  inside the pages\auth folder

--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -1,10 +1,43 @@
-function SignInPage() {
+import { useState } from "react";
+
+const SignInPage = () => {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-        <div className="font-playfair text-green-500">
-            Sign In!
-            <i class="fa-regular fa-face-smile"></i>
+        <div className="flex justify-center">
+            <form className="w-full max-w-sm font-lato p-4 m-4 bg-white border rounded-lg border-slate-200">
+                <div className="flex flex-col my-4">
+                    <label htmlFor="username" className="text-slate-500 pl-1">
+                        username
+                    </label>
+                    <input
+                        id="username"
+                        type="text"
+                        value={userName}
+                        onChange={(e) =>
+                            setUserName(e.target.value)
+                        }
+                        className="border rounded-lg bg-slate-50 border-slate-200 px-2 py-1 focus:outline-emerald-600" />
+                </div>
+                <div className="flex flex-col my-4">
+                    <label htmlFor="password" className="text-slate-500 pl-1">
+                        password
+                    </label>
+                    <input
+                        id="password"
+                        type="password"
+                        value={password}
+                        onChange={(e) =>
+                            setPassword(e.target.value)
+                        }
+                        className="border rounded-lg bg-slate-50 border-slate-200 px-2 py-1 focus:outline-emerald-600" />
+                </div>
+                <button className="w-full text-white bg-emerald-700 rounded-lg py-4 mt-4">
+                    Sign in
+                </button>
+            </form>
         </div>
     )
 }
-
-export default SignInPage
+export default SignInPage;
--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -1,9 +1,57 @@
-function SignUpPage() {
+import { useState } from "react";
+
+const SignInPage = () => {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
     return (
-        <div className="font-lato text-blue-500">
-            Sign Up! 
+        <div className="flex justify-center">
+            <form className="w-full max-w-sm font-lato p-4 m-4 bg-white border rounded-lg border-slate-200">
+                <div className="flex flex-col my-4">
+                    <label htmlFor="username" className="text-slate-500 pl-1">
+                        username
+                    </label>
+                    <input
+                        id="username"
+                        type="text"
+                        value={userName}
+                        onChange={(e) =>
+                            setUserName(e.target.value)
+                        }
+                        className="border rounded-lg bg-slate-50 border-slate-200 px-2 py-1 focus:outline-emerald-600" />
+                </div>
+                <div className="flex flex-col my-4">
+                    <label htmlFor="password" className="text-slate-500 pl-1">
+                        password
+                    </label>
+                    <input
+                        id="password"
+                        type="password"
+                        value={password}
+                        onChange={(e) =>
+                            setPassword(e.target.value)
+                        }
+                        className="border rounded-lg bg-slate-50 border-slate-200 px-2 py-1 focus:outline-emerald-600" />
+                </div>
+                <div className="flex flex-col my-4">
+                    <label htmlFor="confirm-password" className="text-slate-500 pl-1">
+                        confirm password
+                    </label>
+                    <input
+                        id="confirm-password"
+                        type="password"
+                        value={confirmPassword}
+                        onChange={(e) =>
+                            setConfirmPassword(e.target.value)
+                        }
+                        className="border rounded-lg bg-slate-50 border-slate-200 px-2 py-1 focus:outline-emerald-600" />
+                </div>
+                <button className="w-full text-white bg-emerald-700 rounded-lg py-4 mt-4">
+                    create account
+                </button>
+            </form>
         </div>
     )
 }
-
-export default SignUpPage
+export default SignInPage;
